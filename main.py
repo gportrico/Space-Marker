f5e81f8daefdbae307f999ea6eeb91dc31f6b754
Gabriel Rico <1136215@atitus.edu.br>
2024-06-26 16:31:22 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import tkinter as tk
+from tkinter import simpledialog
+import json
+import os
+import random
+
+pygame.init()
+pygame.mixer.init()
+
+#SETUP
+tamanho_janela = (1000, 560)
+tela = pygame.display.set_mode(tamanho_janela)
+pygame.display.set_caption("SPACE MARKER")
+imagem_fundo = pygame.image.load("assets/bg.jpg")
+icone = pygame.image.load("assets/space.png")
+pygame.display.set_icon(icone)
+pygame.mixer.music.load("assets/Space_Machine_Power.mp3")
+pygame.mixer.music.play(-1)
+fonte = pygame.font.Font(None, 24)
+
+marcacoes = {}
+posicoes_estrelas = []
+
+BRANCO = (255, 255, 255)
+PRETO = (0, 0, 0)
+
