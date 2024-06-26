7c17b5864e62ded63fb398af405e22b04b2c66a9
Gabriel Rico <1136215@atitus.edu.br>
2024-06-26 16:45:56 -0300

    Save e Load do game

--- main.py
+++ main.py
@@ -33,3 +33,50 @@ def obter_nome_estrela():
     root.destroy()
     return nome_estrela if nome_estrela else "Desconhecido"
 
+#SAVE
+def salvar_marcacoes():
+    data = {
+        "estrelas": []
+    }
+    for pos, nome in marcacoes.items():
+        dado_estrela = {
+            "nome": nome,
+            "posicao": pos
+        }
+        data["estrelas"].append(dado_estrela)
+    
+    try:
+        with open("assets/marcacoes.json", "w") as f:
+            json.dump(data, f, indent=4)
+        print("Marcacoes salvas com sucesso.")
+    except Exception as e:
+        print(f"Erro ao salvar marcacoes: {e}")
+
+#LOAD
+def carregar_marcacoes():
+    global marcacoes, posicoes_estrelas
+    try:
+        with open("assets/marcacoes.json", "r") as f:
+            data = json.load(f)
+            marcacoes.clear()
+            posicoes_estrelas.clear()
+            for dado_estrela in data["estrelas"]:
+                pos = tuple(dado_estrela["posicao"])
+                nome = dado_estrela["nome"]
+                marcacoes[pos] = nome
+                posicoes_estrelas.append(pos)
+            
+    except Exception as e:
+        print(f"Erro ao carregar marcacoes: {e}")
+        
+#Limpar Estrelas
+def limpar_tela():
+    global marcacoes, posicoes_estrelas
+    marcacoes.clear()
+    posicoes_estrelas.clear()    
+
+#Limpar Save
+def excluir_marcacoes_e_arquivo():
+    limpar_tela()
+    if os.path.exists("assets/marcacoes.json"):
+        os.remove("assets/marcacoes.json")
